0148653da13d0163039bc3f5a8113663f57da5ea
sasitha <sasithadevindawasala@gmail.com>
2023-08-08 11:50:19 +0530

    order service

--- order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -1,17 +1,21 @@
 package com.programmingtechie.orderservice.controller;
 
 import com.programmingtechie.orderservice.dto.request.OrderRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.programmingtechie.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
+   private final OrderService orderService;
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest)
-    {
+    {   orderService.placeOrder(orderRequest);
         return "Oder Created Successfully";
     }
 }
--- order-service/src/main/java/com/programmingtechie/orderservice/dto/request/OrderRequest.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/dto/request/OrderRequest.java
@@ -2,9 +2,14 @@ package com.programmingtechie.orderservice.dto.request;
 
 import com.programmingtechie.orderservice.dto.other.OrderLineItemsDTO;
 import com.programmingtechie.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequest {
     private List<OrderLineItemsDTO> orderLineItemsDTOList;
 }
