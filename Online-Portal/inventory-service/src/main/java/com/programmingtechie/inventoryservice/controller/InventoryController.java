284e283841da93ca0df1ba58736c16b4e58593c1
sasitha <sasithadevindawasala@gmail.com>
2023-08-09 23:33:58 +0530

    sqdf

--- Online-Portal/inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
+++ Online-Portal/inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
@@ -16,6 +16,7 @@ public class InventoryController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<InventoryResponse> isInStock(@RequestParam List<String>  skuCode){
+        //new
         return inventoryService.isInStock(skuCode);
 
     }
